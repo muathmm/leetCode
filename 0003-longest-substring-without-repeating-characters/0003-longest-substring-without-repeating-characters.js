ca8a29c320255192d145ca8238d23cb3ddb2f403
muath mhawich <127548222+muathmm@users.noreply.github.com>
2024-10-13 03:29:59 +0300

    Time: 85 ms (55.54%), Space: 53.5 MB (64.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-      let left = 0;
-    let right = 0;
+    let left = 0;
     let maxLength = 0;
     let charSet = new Set();
 
-    while (right < s.length) {
-        if (!charSet.has(s[right])) {
-            charSet.add(s[right]);
-            right++;
-            maxLength = Math.max(maxLength, right - left);
-        } else {
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
             charSet.delete(s[left]);
             left++;
         }
+
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
     }
 
-    return maxLength;
+    return maxLength;    
 };
